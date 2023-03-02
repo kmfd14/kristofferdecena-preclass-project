ba10444166e0816d187eff0b9c4f7efc3e8f7d3d
kmfd14 <kmfd14@gmail.com>
2023-03-02 16:06:09 +0800

    activity_2: function that takes a Hash and returns the full name

--- activities/activity_2.rb
+++ activities/activity_2.rb
@@ -3,3 +3,8 @@
 #  Values will be your respective names.
 #  Write a function that takes a Hash and returns the full name.
 #  Print on console the result.
+def take_my_name(name_hash)
+    puts "#{name_hash[:first]} #{name_hash[:middle]} #{name_hash[:last]}"
+end
+full_name = Hash[first: "Kristoffer", middle: "Foronda" , last: "Decena"]
+take_my_name(full_name)
--- activities/activity_3.rb
+++ activities/activity_3.rb
@@ -2,3 +2,7 @@
 #  Write a function that takes an array of ages.
 #  The function will return an array of age differences between the 1st and 2nd age, 2nd and 3rd  .. so on and so forth.
 #  Print on console the result.
+
+def age_diff(age_values)
+  ages=
+end
