c5ccfd251dd1fa68bc2e2b98555f265b5ce899cc
kmfd14 <kmfd14@gmail.com>
2023-03-02 14:51:03 +0800

    exercise_3: Identify if adult using array of age.

--- exercises/exercise_3.rb
+++ exercises/exercise_3.rb
@@ -1,9 +1,13 @@
-age = [18, 21, 25,30, 45]
 
-def adult_identifier(age)
-  if age >= 18
-    return "Adult"
-  else
-    return "Not Adult"
+
+def adult_identifier(ages)
+  adults =[]
+  ages.each do |age|
+    if age >= 18
+      adults << age
+    end
   end
+  return adults
 end
+
+p adult_identifier([12,18,21,34,17])
